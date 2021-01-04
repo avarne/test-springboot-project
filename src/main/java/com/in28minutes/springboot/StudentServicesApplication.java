f4562df53ef9fc38425180dc8d511e59089ff26e
codestar <avarne@digite.com>
2021-01-04 15:29:40 +0530

    icp:icp-20:Changes of deployment comitted

--- src/main/java/com/in28minutes/springboot/StudentServicesApplication.java
+++ src/main/java/com/in28minutes/springboot/StudentServicesApplication.java
@@ -2,11 +2,18 @@ package com.in28minutes.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class StudentServicesApplication {
+public class StudentServicesApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentServicesApplication.class, args);
 	}
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+                return application.sources(StudentServicesApplication.class);
+    }
 }
